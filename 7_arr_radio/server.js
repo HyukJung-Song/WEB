8226ef7418788b3c32e658e11495f8e85d8b0b29
hyukjung <go_go_ssing@naver.com>
2021-04-22 19:06:19 +0900

    .

--- 7_arr_radio/server.js
+++ 7_arr_radio/server.js
@@ -6,3 +6,7 @@ var server = http.createServer(app).listen(80);
 app.get('/arr', function (req, res) {
   res.sendfile("src/arr.html")
 });
+
+app.get('/radio0', function (req, res) {
+  res.sendfile("src/radio0.html")
+});
