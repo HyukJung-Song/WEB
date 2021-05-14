81bfa7590206e70a6d21a3ce6b8089627b6eab84
hyukjung <go_go_ssing@naver.com>
2021-05-14 20:24:53 +0900

    a

--- 9_0514/server.js
+++ 9_0514/server.js
@@ -3,31 +3,50 @@ var http = require('http');
 var app = express();
 var server = http.createServer(app).listen(80);
 
+let mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database: 'test'
+});
+connection.connect();
 
 app.get('/item', function (req, res) {
   res.sendfile("src/item.html")
 });
 
-app.get('/priceCheck', function(req, res) {
+app.get('/itemCheck', function(req, res) {
+  // console.log(items[0].price)
   // console.log(req.query.price);
-  let price = req.query.price;
-  let canBuy;
-  if (price < 1000) {
-    canBuy = "구매불가"
-  } else if (price < 5000) {
-    canBuy = "item1"
-  } else if (price < 10000) {
-    canBuy = "item2"
-  } else if (price < 30000) {
-    canBuy = "item3"
-  } else if (price < 50000) {
-    canBuy = "item4"
-  } else if (price < 100000) {
-    canBuy = "item5"
-  } else if (price < 500000) {
-    canBuy = "item6"
-  } else{
-    canBuy = "item7"
+
+  let items = [
+    {name:"item1", price:1000},
+    {name:"itemm2", price:5000},
+    {name:"itemmm3", price:10000},
+    {name:"iteem4", price:30000},
+    {name:"itttem5", price:50000},
+    {name:"iiiitem6", price:100000},
+    {name:"iteem7", price:500000}
+  ];
+
+  let itemName = "구매불가";
+  const price = req.query.price;  // const는 변경이 안된다.
+
+  for (i in items) {
+    if (items[i].price <= price) {
+      itemName = items[i].name;
+    }
   }
-  res.send(canBuy)
+  res.send(itemName)
+});
+
+
+app.get('/priceCheck', function(req, res) {
+  connection.query(`SELECT itemPrice
+    FROM item where itemName="${req.query.name}"`,  // ``안에 ${}있으면 {}는 변수로 인식
+    function(error, results, fields) {
+      if (error) throw error;
+      res.send(results)
+  });
 });
